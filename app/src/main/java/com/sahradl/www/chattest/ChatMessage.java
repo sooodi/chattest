4c8becf61270e401e5a48a0d9f6b51f60c7a2f23
soodabeh <sudytaher@gmail.com>
2019-01-11 19:59:12 +0330

    Initial commit

---
+++ app/src/main/java/com/sahradl/www/chattest/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.sahradl.www.chattest;
+
+/**
+ * Created by sudabeh on 4/22/2017.
+ */
+
+public class ChatMessage {
+
+        public boolean left;
+        public String message;
+
+        public ChatMessage(boolean left , String message) {
+            // TODO Auto-generated constructor stub
+            super();
+            this.left=left;
+            this.message = message;
+        }
+    }
+
